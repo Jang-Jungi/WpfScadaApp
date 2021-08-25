c1754b402d3fef0cb6f1d86f34bb7343c373cec0
jjgweb951101@gmail.com <jone1542%%>
2021-08-25 12:37:06 +0900

    초기 커밋.

---
+++ WpfScadaApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WpfScadaApp
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        // 로그 설정
+        public static readonly NLog.Logger LOGGER = NLog.LogManager.GetCurrentClassLogger();
+    }
+}
